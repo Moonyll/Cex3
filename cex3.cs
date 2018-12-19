e51811113df50759b8a1b2e9fde70b5445388910
Moonyll <45941071+Moonyll@users.noreply.github.com>
2018-12-19 14:53:37 +0100

    Update cex3.cs

--- cex3.cs
+++ cex3.cs
@@ -21,3 +21,52 @@ namespace ConsoleApp31
         }
     }
 }
+/* Console.WriteLine("entrer votre Genre M/Mme");
+            string gender = (Console.ReadLine());
+            Console.WriteLine("entrer votre âge");
+            int age = int.Parse(Console.ReadLine());
+            /*if (gender == "M" && age > 17 && age <= 120)    //Condition si Homme ET age entre 18 ET 120 ans
+            {
+                Console.WriteLine("Vous êtes un Homme et vous êtes majeur.");
+            }
+            else if (gender == "M" && age < 18 && age >= 1) //Condition si Homme ET age entre 17 ET 1 ans
+            {
+                Console.WriteLine("Vous êtes un Homme et vous êtes mineur.");
+            }
+            else if (gender == "Mme" && age > 17 && age <= 120)
+            {
+                Console.WriteLine("Vous êtes une Femme et vous êtes majeure.");
+            }
+            else if (gender == "Mme" && age < 18 && age >= 1)
+            {
+                Console.WriteLine("Vous êtes un Femme et vous êtes mineure.");
+            }
+            else { Console.WriteLine("Il doit y avoir un problème"); }*/
+            if (gender == "Mme")
+            {
+                if (age > 17 && age <= 120)
+                {
+                    Console.WriteLine("Vous êtes une Femme et vous êtes majeure.");
+                }
+                else if (age < 18 && age >= 1)
+                {
+                    Console.WriteLine("Vous êtes un Femme et vous êtes mineure.");
+                }
+                else { Console.WriteLine("Il doit y avoir un problème au niveau de l'âge"); }
+            }
+            else if (gender == "M")
+            {
+                if (age > 17 && age <= 120)
+                {
+                    Console.WriteLine("Vous êtes un Homme et vous êtes majeur.");
+                }
+                else if (age < 18 && age >= 1)
+                {
+                    Console.WriteLine("Vous êtes un Homme et vous êtes mineur.");
+                }
+                else { Console.WriteLine("Il doit y avoir un problème au niveau de l'âge"); }
+            }
+            else { Console.WriteLine("Il doit y avoir un problème au niveau du genre"); }
+        }
+    }
+}*/
